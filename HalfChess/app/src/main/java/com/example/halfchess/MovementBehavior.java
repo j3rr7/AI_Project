929f609b92dc16ccb0ae33482bb9015d67abcc0f
Yocucuuu <44639176+Yocucuuu@users.noreply.github.com>
2020-11-03 18:36:28 +0700

    move king && move pawn
    nambahin movement verical ambe movement serong

--- HalfChess/app/src/main/java/com/example/halfchess/MovementBehavior.java
+++ HalfChess/app/src/main/java/com/example/halfchess/MovementBehavior.java
@@ -24,12 +24,13 @@ public interface MovementBehavior {
                     canMove = true;
                 }
 
+
                 //Makan
-                if(x-1 >= 0 && MainActivity.papan[y-1][x-1].getBidak()!=null ){
-                        MainActivity.tiles[y-1][x-1].setBackgroundColor(Color.GREEN);
+                if(x-1 >= 0 && MainActivity.papan[y-1][x-1].getBidak()!=null && !MainActivity.papan[y-1][x-1].getBidak().isP1()){
+                    MainActivity.tiles[y-1][x-1].setBackgroundColor(Color.GREEN);
                     canMove = true;
                 }
-                if(x+1 <= 3 && MainActivity.papan[y-1][x+1].getBidak()!=null ){
+                if(x+1 <= 3 && MainActivity.papan[y-1][x+1].getBidak()!=null && !MainActivity.papan[y-1][x+1].getBidak().isP1() ){
                     MainActivity.tiles[y-1][x+1].setBackgroundColor(Color.GREEN);
                     canMove = true;
                 }
@@ -50,11 +51,11 @@ public interface MovementBehavior {
                 }
 
                 //Makan
-                if(x-1 >= 0 && MainActivity.papan[y+1][x-1].getBidak()!=null ){
+                if(x-1 >= 0 && MainActivity.papan[y+1][x-1].getBidak()!=null && MainActivity.papan[y+1][x-1].getBidak().isP1() ){
                     MainActivity.tiles[y+1][x-1].setBackgroundColor(Color.GREEN);
                     canMove = true;
                 }
-                if(x+1 <= 3 && MainActivity.papan[y+1][x+1].getBidak()!=null ){
+                if(x+1 <= 3 && MainActivity.papan[y+1][x+1].getBidak()!=null && MainActivity.papan[y+1][x+1].getBidak().isP1() ){
                     MainActivity.tiles[y+1][x+1].setBackgroundColor(Color.GREEN);
                     canMove = true;
                 }
@@ -69,20 +70,69 @@ public interface MovementBehavior {
         @Override
         public Boolean Pickup(Boolean player1, int x, int y) {
             Boolean canMove = false;
+            //gerak ke horizontal e tak gabung ambe gerak serong
+            // cuma kopas dari koding sg gerak vertikal tak gabung nde if e horizontal
+
 
             if(x-1>=0){
                 if(MainActivity.papan[y][x-1].getBidak() == null || MainActivity.papan[y][x-1].getBidak().isP1() != player1 ){
                     MainActivity.tiles[y][x-1].setBackgroundColor(Color.GREEN);
                     canMove = true;
                 }
+
+                //serong
+                if(y+1 <=7){
+                    if(MainActivity.papan[y+1][x-1].getBidak() == null || MainActivity.papan[y+1][x-1].getBidak().isP1() != player1 ){
+                        MainActivity.tiles[y+1][x-1].setBackgroundColor(Color.GREEN);
+                        canMove = true;
+                    }
+                }
+                if(y-1>=0){
+                    if(MainActivity.papan[y-1][x-1].getBidak() == null || MainActivity.papan[y-1][x-1].getBidak().isP1() != player1 ){
+                        MainActivity.tiles[y-1][x-1].setBackgroundColor(Color.GREEN);
+                        canMove = true;
+                    }
+                }
+                //end serong
             }
 
-            if(x+1 <=7){
+            if(x+1 <=3){
                 if(MainActivity.papan[y][x+1].getBidak() == null || MainActivity.papan[y][x+1].getBidak().isP1() != player1 ){
                     MainActivity.tiles[y][x+1].setBackgroundColor(Color.GREEN);
                     canMove = true;
                 }
+
+                //serong
+                if(y+1 <=7){
+                    if(MainActivity.papan[y+1][x+1].getBidak() == null || MainActivity.papan[y+1][x+1].getBidak().isP1() != player1 ){
+                        MainActivity.tiles[y+1][x+1].setBackgroundColor(Color.GREEN);
+                        canMove = true;
+                    }
+                }
+                if(y-1>=0){
+                    if(MainActivity.papan[y-1][x+1].getBidak() == null || MainActivity.papan[y-1][x+1].getBidak().isP1() != player1 ){
+                        MainActivity.tiles[y-1][x+1].setBackgroundColor(Color.GREEN);
+                        canMove = true;
+                    }
+                }
+                //end serong
             }
+
+            // vertikal
+            if(y+1 <=7){
+                if(MainActivity.papan[y+1][x].getBidak() == null || MainActivity.papan[y+1][x].getBidak().isP1() != player1 ){
+                    MainActivity.tiles[y+1][x].setBackgroundColor(Color.GREEN);
+                    canMove = true;
+                }
+            }
+
+            if(y-1>=0){
+                if(MainActivity.papan[y-1][x].getBidak() == null || MainActivity.papan[y-1][x].getBidak().isP1() != player1 ){
+                    MainActivity.tiles[y-1][x].setBackgroundColor(Color.GREEN);
+                    canMove = true;
+                }
+            }
+
             return canMove;
         }
     }
