008e9aafaed2d161f3d21515238329e1bb13d3cd
Jeremy <adgrygry12348@gmail.com>
2020-11-25 18:37:45 +0700

    Create AI.java

---
+++ HalfChess/app/src/main/java/com/example/halfchess/AI.java
@@ -0,0 +1,18 @@
+package com.example.halfchess;
+
+public class AI {
+	class Node{
+		Papan state[][];
+		int value;
+		
+		Node(Papan[][] state, int val)
+		{
+			this.state = state;
+			this.value = val;
+		}
+	}
+	
+	class AIBehaviour{
+		// ToDo
+	}
+}
